fd7e62cc3018b649db45abf7f001d2104ed0a728
K1ngmar <ingmarsven.kole@gmail.com>
2024-12-03 21:46:28 +0100

    slightly improved code

--- day02/solution.cpp
+++ day02/solution.cpp
@@ -11,48 +11,49 @@
 int main(void)
 {
 	std::ifstream inputFile("input.txt");
+	std::stringstream buffer;
+	buffer << inputFile.rdbuf();
+
+	const auto input = buffer.str();
 
 	size_t resultEx00 = 0;
 	size_t resultEx01 = 0;
+	size_t x = 0;
+	size_t y = 0;
+
+	const std::regex multiplyRegex(
+		"(m)(u)(l)(\\()\\d{1,3},\\d{1,3}(\\))"
+		"|"
+		"(d)(o)(\\()(\\))"
+		"|"
+		"(d)(o)(n)(')(t)(\\()(\\))"
+	);
+	const std::sregex_iterator matchesBegin(input.begin(), input.end(), multiplyRegex);
+	const std::sregex_iterator matchesEnd;
+
 	bool ignoreMultiplies = false;
-	for (std::string line; std::getline(inputFile, line);)
+	for (auto match = matchesBegin; match != matchesEnd; match++)
 	{
-		size_t x = 0;
-		size_t y = 0;
-
-		std::regex multiplyRegex(
-			"(m)(u)(l)(\\()\\d{1,3},\\d{1,3}(\\))"
-			"|"
-			"(d)(o)(\\()(\\))"
-			"|"
-			"(d)(o)(n)(')(t)(\\()(\\))"
-		);
-		const auto matchesBegin = std::sregex_iterator(line.begin(), line.end(), multiplyRegex);
-		const auto matchesEnd = std::sregex_iterator();
-	
-		for (auto match = matchesBegin; match != matchesEnd; match++)
+		if (match->str() == "do()")
 		{
-			if (match->str() == "do()")
-			{
-				ignoreMultiplies = false;
-				continue;
-			}
-			if (match->str() == "don't()")
-			{
-				ignoreMultiplies = true;
-				continue;
-			}
-
-			std::sscanf(match->str().data(), "mul(%zu,%zu)", &x, &y);
-			resultEx00 += (x * y);
-
-			if (ignoreMultiplies)
-			{
-				continue;
-			}
-		
-			resultEx01 += (x * y);
+			ignoreMultiplies = false;
+			continue;
 		}
+		if (match->str() == "don't()")
+		{
+			ignoreMultiplies = true;
+			continue;
+		}
+
+		std::sscanf(match->str().data(), "mul(%zu,%zu)", &x, &y);
+		resultEx00 += (x * y);
+
+		if (ignoreMultiplies)
+		{
+			continue;
+		}
+	
+		resultEx01 += (x * y);
 	}
 	std::cout << "ex00: " << resultEx00 << std::endl;
 	std::cout << "ex01: " << resultEx01 << std::endl;
