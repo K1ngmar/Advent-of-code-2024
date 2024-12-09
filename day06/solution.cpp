5bf56228d44366b53d67c7858b81f0b4bc23879f
K1ngmar <ingmarsven.kole@gmail.com>
2024-12-09 20:24:13 +0100

    part 2

--- day06/solution.cpp
+++ day06/solution.cpp
@@ -10,50 +10,70 @@
 #include <algorithm>
 #include <cctype>
 
-static bool CouldBeValidEquation(const std::vector<size_t>& equationValues, size_t testValue)
+class Calculator
 {
+    bool isPlusEnabled = false;
+    bool isMultiplyEnabled = false;
+    bool isConcatenateEnabled = false;
 
-    constexpr auto possibleOperators = "+*";
-    std::string stringToDoPermutationsOn;
-    for (const auto& s : std::views::repeat(possibleOperators, equationValues.size() - 1))
+    const std::vector<size_t> equationValues;
+    const size_t testValue;
+
+    using itr = std::vector<size_t>::const_iterator;
+
+    bool TryEquation(itr i, size_t currentValue)
     {
-        // Actually checking way more permutations than necessary but this required less thinking :)
-        stringToDoPermutationsOn.append(s);
-    }
-    std::sort(stringToDoPermutationsOn.begin(), stringToDoPermutationsOn.end());
-
-
-    bool previousPermutation = true;
-    while (true)
-    {
-        size_t currentVal = equationValues.front();
-        for (const auto& [operatorIndex, value] : std::views::drop(equationValues, 1) | std::views::enumerate)
-        {
-            if (stringToDoPermutationsOn.at(operatorIndex) == '+') {
-                currentVal += value;
-            }
-            else {
-                currentVal *= value;
-            }
-        }
-
-        if (currentVal == testValue) {
+        if (currentValue == testValue) {
             return true;
         }
-
-        if (!previousPermutation) {
+        if (i == equationValues.end()) {
             return false;
         }
 
-        // Need to figure out how to do this properly because this is ugly.
-        previousPermutation = std::next_permutation(stringToDoPermutationsOn.begin(), stringToDoPermutationsOn.end());
+        auto val = *i;
+        if (isPlusEnabled && TryEquation(i + 1, currentValue + val)) {
+            return true;
+        }
+        if (isMultiplyEnabled && TryEquation(i + 1, currentValue * val)) {
+            return true;
+        }
+        if (isConcatenateEnabled)
+        {
+            while (val > 0)
+            {
+                currentValue = (currentValue * 10) + (val % 10);
+                val /= 10;
+            }
+            if (TryEquation(i + 1, currentValue)) {
+                return true;
+            }
+        }
+        return false;
     }
-}
+
+public:
+
+    Calculator(const std::vector<size_t>& equationValues_, size_t testValue_)
+    : equationValues(equationValues_)
+    , testValue(testValue_)
+    {}
+
+    bool IsValidEquationWithOperators(const std::string& operatorsToCheck)
+    {
+        isPlusEnabled = operatorsToCheck.contains('+');
+        isMultiplyEnabled = operatorsToCheck.contains('*');
+        isConcatenateEnabled = operatorsToCheck.contains('|');
+
+        return TryEquation(equationValues.begin(), 0);
+    }
+
+};
 
 int main(void)
 {
 	std::ifstream inputFile("input.txt");
 
+    size_t resultEx00 = 0;
     size_t resultEx01 = 0;
     size_t i = 0;
     for (std::string line; std::getline(inputFile, line);)
@@ -70,16 +90,20 @@ int main(void)
             }
             equationValues.emplace_back(std::stoull(std::string_view(value).begin()));
         }
-        if (CouldBeValidEquation(equationValues, testValue))
-        {
-            std::cout << i << ": " << testValue <<  std::endl;
+        Calculator calculator(equationValues, testValue);
+        if (calculator.IsValidEquationWithOperators("+*")) {
+            resultEx00 += testValue;
+        }
+        if (calculator.IsValidEquationWithOperators("+*|")) {
+            std::cout << i << ": " << testValue << std::endl;
             resultEx01 += testValue;
         }
         i++;
         equationValues.clear();
     }
 
-	std::cout << "ex01: " << resultEx01 << std::endl;
+	std::cout << "ex00: " << resultEx00 << std::endl;
+    std::cout << "ex01: " << resultEx01 << std::endl;
 
     return 0;
 }
